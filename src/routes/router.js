841b34d4a1731ea5aca5a0d4c33e509d802c742d
husnulpadilla <Husnulpadilla@gmail.com>
2025-07-08 08:18:21 +0700

    update db.json and create router

---
+++ src/routes/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Laporan from "@/views/Laporan.vue";
+import Transaksi from "@/views/Transaksi.vue";
+import Home from "@/views/Home.vue";
+import Riwayat from "@/views/Riwayat.vue";
+import Pengerjaan from "@/views/Pengerjaan.vue";
+import Terapis from "@/views/Terapis.vue";
+
+const routes = [
+    {path: '/', name: 'home', component: Home},
+    {path: '/pengerjaan', name:'pengerjaan', component: Pengerjaan},
+    {path: '/riwayat', name:'riwayat', component: Riwayat},
+    {path: '/transaksi', name:'transaksi', component: Transaksi},
+    {path: '/laporan', name: 'laporan', component: Laporan},
+    {path: '/terapis', name: 'terapis', component: Terapis}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
